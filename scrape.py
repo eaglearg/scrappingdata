db46147c5b635aa90f8941261008b47197817ea5
Diego Ormaza <ormazadepaul@gmail.com>
2019-10-29 17:50:37 -0300

    added new ways to call it

--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 def get_pages_hn(total_pages):
@@ -26,4 +27,18 @@ def create_custom_hn(links, subtext):
                 hn.append({'title': title, 'link': href, 'votes': points })    
     return sort_stories_by_votes(hn)
 
-print(get_pages_hn(10))
+if __name__ == '__main__':
+    try:
+        if int(sys.argv[1]):
+            print(get_pages_hn(int(sys.argv[1])))
+    except (IndexError, ValueError) as err:
+        while True:
+            try:
+                pages_to_scrap = int(input("how many pages do you want to scrape? "))
+                if pages_to_scrap > 0:
+                    print(get_pages_hn(pages_to_scrap))
+                    break
+                else:
+                    print("you must enter a number greater than 0")
+            except ValueError as err:
+                print("you must enter a number greater than 0")
